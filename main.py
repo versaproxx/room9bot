fa3dd99c358490824f3b5d339cbfecc4a02127b1
Kseniya Prokofeva <silencio_pr@protonmail.com>
2022-12-04 23:33:26 +0300

    dded nahuy function

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ import time
 bot = telebot.TeleBot(secrets.get('BOT_TOKEN'))
 
 pidor_list = []
-phraseList = [f'Кто же этот пидор, что спиздил у меня головку на {random.randint(8,32)}', 'Список не большой', 'Сейчас посмотрим']
+phraseList = [f'Кто же этот пидор, что спиздил у меня головку на {random.randint(8, 32)}', 'Список не большой',
+              'Сейчас посмотрим']
+
 
 def pidorList():
     with open(r'users.csv') as f:
@@ -18,17 +20,20 @@ def pidorList():
         for row in reader:
             pidor_list.append(row[0])
     return pidor_list
+
+
 pidorList()
 
-def getPidor():
+
+def get_pidor():
     pidor_of_the_day = random.choice(pidor_list)
-    with open (r'restrictedDates.csv', mode='a', newline='\n', encoding='utf-8') as dateFile:
-        dateWriter = csv.writer(dateFile, delimiter=',', quotechar='"')
-        dateWriter.writerow([datetime.datetime.today().strftime('%Y-%m-%d'), pidor_of_the_day])
+    with open(r'restrictedDates.csv', mode='a', newline='\n', encoding='utf-8') as dateFile:
+        date_writer = csv.writer(dateFile, delimiter=',', quotechar='"')
+        date_writer.writerow([datetime.datetime.today().strftime('%Y-%m-%d'), pidor_of_the_day])
     return pidor_of_the_day
 
 
-def getDate():
+def get_date():
     last_played_date = ''
     with open(r'restrictedDates.csv') as f:
         reader = csv.reader(f)
@@ -37,64 +42,72 @@ def getDate():
             print(row[0])
     return last_played_date
 
-getDate()
-def registerPidor(msg):
-        with open(r'users.csv', mode='a', newline='\n', encoding='utf-8') as users_file:
-            pidor_writer = csv.writer(users_file, delimiter=',', quotechar='"')
-            pidor_writer.writerow([msg.from_user.username, 0])
-        pidor_list.append(msg.from_user.username)
-        bot.send_message(msg.chat.id, f'Вы добавлены в игру, {msg.from_user.first_name} (@{msg.from_user.username})')
 
-def getPidorToday(msg):
-        with open(r'restrictedDates.csv') as f:
-            temp_pidor_list = []
-            reader = csv.reader(f)
-            for row in reader:
-                temp_pidor_list.append(row[1])
-        bot.send_message(msg.chat.id, f'Пидор дня: {temp_pidor_list[-1]}')
+get_date()
+
+
+def register_pidor(msg):
+    with open(r'users.csv', mode='a', newline='\n', encoding='utf-8') as users_file:
+        pidor_writer = csv.writer(users_file, delimiter=',', quotechar='"')
+        pidor_writer.writerow([msg.from_user.username, 0])
+    pidor_list.append(msg.from_user.username)
+    bot.send_message(msg.chat.id, f'Вы добавлены в игру, {msg.from_user.first_name} (@{msg.from_user.username})')
+
+
+def get_pidor_today(msg):
+    with open(r'restrictedDates.csv') as f:
+        temp_pidor_list = []
+        reader = csv.reader(f)
+        for row in reader:
+            temp_pidor_list.append(row[1])
+    bot.send_message(msg.chat.id, f'Пидор дня: {temp_pidor_list[-1]}')
 
 
 @bot.message_handler(commands=["pidor"])
 def start(m, res=False):
     bot.send_message(m.chat.id, f'А может ты пидор ?')
 
+
 @bot.message_handler(commands=['pidor_reg'])
 def start(msg, res=False):
     pidorList()
     if msg.from_user.username in pidor_list:
-                 bot.send_message(msg.chat.id, 'Эй! Ты уже в игре!')
+        bot.send_message(msg.chat.id, 'Эй! Ты уже в игре!')
     else:
-        registerPidor(msg)
-        
-            
+        register_pidor(msg)
+
+
 @bot.message_handler(commands=['pidor_list'])
 def start(msg, res=False):
     stringa = ','.join(pidor_list)
     bot.send_message(msg.chat.id, stringa)
 
+
 @bot.message_handler(commands=['find_pidor'])
 def start(msg, res=False):
-    if datetime.datetime.strptime(getDate(),'%Y-%m-%d').date() >= datetime.datetime.today().date():
-        getPidorToday(msg)
-    elif datetime.datetime.strptime(getDate(),'%Y-%m-%d').date() < datetime.datetime.today().date():
+    if datetime.datetime.strptime(get_date(), '%Y-%m-%d').date() >= datetime.datetime.today().date():
+        get_pidor_today(msg)
+    elif datetime.datetime.strptime(get_date(), '%Y-%m-%d').date() < datetime.datetime.today().date():
         for phrase in phraseList:
             bot.send_message(msg.chat.id, phrase)
             time.sleep(3)
-        bot.send_message(msg.chat.id, f'Новый пидор дня сегодня: @{getPidor()}')
-        
+        bot.send_message(msg.chat.id, f'Новый пидор дня сегодня: @{get_pidor()}')
+
+
 message_template = "Иди на хуй, {}"
 
+
 @bot.message_handler(commands=["nah", "nahuy", "nahui", "idinahui", "idinahuy"])
-def idi_na_huy(msg, res=False):
-    message_template = "Иди на хуй, @{}"
-    name = msg.from_user.username
+def idi_na_huy(msg):
+    deleting_message = msg.message_id
     if '@' in str(msg.text):
         name = str(msg.text).split('@')[1]
-        bot.send_message(msg.chat.id, message_template.format(name))
+        bot.reply_to(msg.reply_to_message.from_user.id, f'Иди на хуй, @{name}')
     else:
-        bot.send_message(msg.chat.id, message_template.format(name))
-    bot.delete_message(msg.chat.id, msg.message_id)
+        name = msg.reply_to_message.from_user.username
+        bot.send_message(msg.chat.id, f'Иди на хуй, @{name}',  reply_to_message_id=msg.reply_to_message.message_id)
+
+    bot.delete_message(msg, deleting_message)
 
 
-
-bot.polling(none_stop=True, interval=0) 
+bot.polling(none_stop=True, interval=0)
