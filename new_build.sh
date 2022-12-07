80fa4996d642843c99c4199ed629e585707da0f0
Evgeny Boytsov <32868527+fcpzutpw@users.noreply.github.com>
2022-12-08 01:54:18 +0400

    Auto remove old container and pull new build.

---
+++ new_build.sh
@@ -0,0 +1,25 @@
+#/bin/bash
+
+echo "Trying pull repository"
+git pull
+echo "Git repository pulled"
+
+echo "Stoping all bot containers"
+docker stop $(docker ps -aq)
+echo "All containers stoped"
+
+echo "Deleting old container versions"
+docker stop $(docker ps -aq)
+echo "Deleted"
+
+echo "Delete all images of bot"
+docker rmi $(docker images -q)
+echo "Images deleted"
+
+echo "Starting build new bot version"
+docker build -t 9bot .
+echo "New version of bot builded with name 9bot"
+
+echo "Starting new container"
+docker run -v /opt/bot_files:/opt/9bot/bot_files 9bot
+echo "Check your new version of bot in chat"
