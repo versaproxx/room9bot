21e07bbeafda05dc6285f67795c0312605fdc9df
AlKab <kabanoff90@gmail.com>
2022-12-08 01:47:48 +0300

    fix

--- modules/vpizdu.py
+++ modules/vpizdu.py
@@ -8,7 +8,7 @@ def idi_v_pizdu(msg, bot):
             name = msg.reply_to_message.from_user.username
             bot.send_message(msg.chat.id, f'Иди в пизду, @{name}',  reply_to_message_id=msg.reply_to_message.message_id)
         except:
-            bot.send_message(msg.chat.id, f'Ой идите вы все в пизду, дамы и господа!') 
+            bot.send_message(msg.chat.id, f'Ой идите вы все в пизду, дамы и господа!!') 
         
 
     bot.delete_message(msg.chat.id, deleting_message)
