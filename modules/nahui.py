0841aeb6e653d8ed460cdd783549d8251f02a3a8
AlKab <kabanoff90@gmail.com>
2022-12-08 13:05:44 +0300

    Final fix for nahuoi and vpizdu

--- modules/nahui.py
+++ modules/nahui.py
@@ -1,8 +1,14 @@
 def idi_na_huy(msg, bot):
     deleting_message = msg.message_id
     if '@' in str(msg.text):
+        print('gavna')
         name = str(msg.text).split('@')[1]
-        bot.reply_to(msg.reply_to_message.from_user.id, f'Иди на хуй, @{name}')
+        try:
+            bot.reply_to(msg.reply_to_message.from_user.id, f'Иди на хуй, @{name}')
+        except Exception as e: 
+            print('internal exc')
+            print(e)
+            bot.send_message(msg.chat.id, f'Да пошли вы все нахуй')
     else:
         try:
             name = msg.reply_to_message.from_user.username
--- modules/vpizdu.py
+++ modules/vpizdu.py
@@ -2,14 +2,19 @@ def idi_v_pizdu(msg, bot):
     deleting_message = msg.message_id
     if '@' in str(msg.text):
         name = str(msg.text).split('@')[1]
-        bot.reply_to(msg.reply_to_message.from_user.id, f'Иди в пизду, @{name}')
+        try:
+            bot.reply_to(msg.reply_to_message.from_user.id, f'Иди в пизду, @{name}')
+        except Exception as e: 
+            print('internal exc')
+            print(e)
+            bot.send_message(msg.chat.id, f'Ой идите вы все в пизду, дамы и господа!')
     else:
         try:
             name = msg.reply_to_message.from_user.username
             bot.send_message(msg.chat.id, f'Иди в пизду, @{name}',  reply_to_message_id=msg.reply_to_message.message_id)
         except Exception as e: 
             print(e)
-            bot.send_message(msg.chat.id, f'Ой идите вы все в пизду, дамы и господа!!') 
+            bot.send_message(msg.chat.id, f'Ой идите вы все в пизду, дамы и господа!') 
         
 
     bot.delete_message(msg.chat.id, deleting_message)
